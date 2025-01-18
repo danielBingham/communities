4ee2be0890dccd8ece8db7a0ee11c26748be9ef0
Daniel Bingham <dbingham@theroadgoeson.com>
2025-01-18 09:43:25 -0500

    Issue #5 -- Give UserRelationships their own state and resource.
    This is to fix the bug in friends.  Keeping UserRelationships as a
    special snowflake that lives on the user session is the root cause of
    several bugs.  Giving UserRelationships its own state and REST resource
    will help resolve those bugs and keep more consistent patterns. There's
    really no reason not to.  I tied myself in a bit of a knot when I
    originally decided not to.

---
+++ packages/backend/migrations/RelationshipsOwnStateMigration.js
@@ -0,0 +1,103 @@
+/******************************************************************************
+ *
+ *  Communities -- Non-profit, cooperative social media 
+ *  Copyright (C) 2022 - 2024 Daniel Bingham 
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
+module.exports = class RelationshipsOwnStateMigration {
+
+    constructor(core) {
+        this.database = core.database
+        this.logger = core.logger
+        this.config = core.config
+    }
+
+
+    /**
+     * Do any setup necessary to initialize this migration.
+     *
+     * This *should not impact data*.  Only steps that have no impact or no
+     * risk to user data should happen here.
+     *
+     * These steps also need to be fully backwards compatible, and
+     * non-destructively rolled back.
+     *
+     * So here we can create a new table, add a column with null values to a
+     * table (to be populated later), create a new ENUM, etc.
+     */
+    async initialize() {
+        try {
+            await this.database.query(`ALTER TABLE user_relationships DROP CONSTRAINT IF EXISTS user_relationships_pkey`, [])
+            await this.database.query(`ALTER TABLE user_relationships ADD COLUMN IF NOT EXISTS id uuid DEFAULT gen_random_uuid()`, [])
+            await this.database.query(`ALTER TABLE user_relationships ADD CONSTRAINT IF NOT EXISTS user_relationships_pkey PRIMARY KEY(id)`, [])
+
+            await this.database.query(`CREATE INDEX IF NOT EXISTS user_relationships__user_id ON user_relationships (user_id)`, [])
+            await this.database.query(`CREATE INDEX IF NOT EXISTS user_relationships__friend_id ON user_relationships (friend_id)`, [])
+        } catch (error) {
+            try {
+                await this.database.query(`DROP INDEX IF EXISTS user_relationships__user_id`, [])
+                await this.database.query(`DROP INDEX IF EXISTS user_relationships__friend_id`, [])
+
+                await this.database.query(`ALTER TABLE user_relationships DROP CONSTRAINT IF EXISTS user_relationships_pkey`, [])
+                await this.database.query(`ALTER TABLE user_relationships DROP COLUMN IF EXISTS id`, [])
+                await this.database.query(`ALTER TABLE user_relationships ADD CONSTRAINT IF NOT EXISTS user_relationships_pkey PRIMARY KEY(user_id, friend_id)`, [])
+            } catch (rollbackError) {
+                throw new MigrationError('no-rollback', rollbackError.message)
+            }
+            throw new MigrationError('rolled-back', error.message)
+        }
+    }
+
+    /**
+     * Rollback the setup phase.
+     */
+    async uninitialize() {
+        try {
+            await this.database.query(`DROP INDEX IF EXISTS user_relationships__user_id`, [])
+            await this.database.query(`DROP INDEX IF EXISTS user_relationships__friend_id`, [])
+
+            await this.database.query(`ALTER TABLE user_relationships DROP CONSTRAINT IF EXISTS user_relationships_pkey`, [])
+            await this.database.query(`ALTER TABLE user_relationships DROP COLUMN IF EXISTS id`, [])
+            await this.database.query(`ALTER TABLE user_relationships ADD CONSTRAINT IF NOT EXISTS user_relationships_pkey PRIMARY KEY(user_id, friend_id)`, [])
+        } catch (error) {
+            try {
+                await this.database.query(`ALTER TABLE user_relationships DROP CONSTRAINT IF EXISTS user_relationships_pkey`, [])
+                await this.database.query(`ALTER TABLE user_relationships ADD COLUMN IF NOT EXISTS id uuid DEFAULT gen_random_uuid()`, [])
+                await this.database.query(`ALTER TABLE user_relationships ADD CONSTRAINT IF NOT EXISTS user_relationships_pkey PRIMARY KEY(id)`, [])
+
+                await this.database.query(`CREATE INDEX IF NOT EXISTS user_relationships__user_id ON user_relationships (user_id)`, [])
+                await this.database.query(`CREATE INDEX IF NOT EXISTS user_relationships__friend_id ON user_relationships (friend_id)`, [])
+            } catch (rollbackError) {
+                throw new MigrationError('no-rollback', rollbackError.message)
+            }
+            throw new MigrationError('rolled-back', error.message)
+        }
+    }
+
+    /**
+     * Execute the migration for a set of targets.  Or for everyone if no
+     * targets are given.
+     *
+     * Migrations always need to be non-destructive and rollbackable.  
+     */
+    async up(targets) { }
+
+    /**
+     * Rollback the migration.  Again, needs to be non-destructive.
+     */
+    async down(targets) { }
+}
