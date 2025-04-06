af490f59bb1bdbe0eda3ed0a71e9ad084a9e2648
Daniel Bingham <daniel.bingham@flexa.co>
2025-04-06 11:41:59 -0400

    Overhaul of the about pages.

--- web-application/client/pages/about/views/About.js
+++ web-application/client/pages/about/views/About.js
@@ -14,39 +14,41 @@ const About = function({}) {
 
     return (
         <article className="about-view">
-            Communities is a user-supported <strong>non-profit social network</strong>.     
-            Our goal is to help people build community,
-            connect, and organize. <br />
-            <p>We're not taking
-                venture capital. We won't show you ads or suggested post spam. We're not selling your
-                data.</p>
-            <p>We want to <strong>de-enshittify the internet.</strong></p>
-            <div className="beta">
-                We're currently in invite-only beta.  If you have an invite, register through the link in your email. Beta means there will be bugs, please use at your own risk.
-            </div>
-
-            <h1>How It Works</h1>
-            <p>Communities currently supports text posts that can have either a single link or a single image attached.</p>
-            <p>Posts are only visible to friends, who can comment or react to them.</p>
-            <p>Posts may be up to 10,000 characters long.  Comments may be up to 5,000 characters.</p>
-            <p>Posts are viewable in either an algorithmic feed or a chronological one.</p>
-            <p>Friends may react to posts using "Like", "Dislike", or "Block".</p>
-            <p>Reactions and comments both affect the post's rank in the
-            algorithmic feed.  Likes, Dislikes, and comments are positive, increasing the
-        post's rank.  Blocks are negative, intended for community self-moderation, and
-                    decrease the post's rank.</p> 
-            <p>If you Block and comment, the comment will not count toward a post's rank and you are encouraged to comment on posts you Block to explain your reasons, <em>provided you feel safe doing so.</em></p>
-            <p>Reactions are not anonymous, so the poster and others can see how you reacted.</p>
-
-            <h3>Healthier Social Media (Work in Progress)</h3>
-            <p>We've made a number of decisions aimed at improving the health of social media.</p>
-            <p><strong>Viewed posts aren't hidden.</strong>  Posts you've already seen are not removed from either of your feeds.  This is both to help you find them again, and so that you can view all of the posts and feel confident that you have.</p>
-            <p><strong>No infinite scroll.</strong>  We intentionally chose to use classic paging in order to build in a break point and give you a chance to decide to walk away when you need to.</p>
-            <p><strong>Comment notifications only.</strong> You only receive notifications for comments, not reactions.  We aren't looking to dopamine addict you, only let you know when there's a comment you might want to respond to.</p>
-            <p>We'll continue seeking feedback, studying the research, and trying to find ways to make this critical communication tool healthier.</p>
-            <h3>We Need Your Feedback!</h3>
-            <p>Communities is currently in beta.  We want feedback on everything, but especially the algorithm.  Please share your thoughts with <a href="mailto:contact@communities.social">contact@communities.social</a>.</p>
-            <p>In the future, we plan to build a space on the platform itself for open meta-discussion of the platform and its systems.</p>
+            <p>Communities is a user-supported social media platform that will be a
+                non-profit, multi-stakeholder cooperative.</p>
+            <p>Communities is a platform designed to enable real, social
+                connection. It's a platform where you can find communities of
+                place and interest.  Where you can organize, collaborate,
+                communicate and then gather in the real world.  It's
+                intentionally designed _not_ to addict you or hold your
+                attention. Rather it's designed to allow you to see the
+                updates that are important to you, have interesting and
+                fulfilling conversations, and then walk away.</p>
+            <p>We're not taking venture capital (or any equity investment).
+            We're not going to show you ads or suggested post spam.  We're not
+                going to sell your data. And we'll write those constraints
+                into the bylaws.</p>
+            <p>It will be supported by you -- its users -- through a sliding
+            scale subscription.  All users are asked to <a
+                href="/about/contribute">contribute $10/month</a> if they can,
+                but the scale goes all the way down to zero if
+                you need it to. You don't have to contribute to use the
+                platform.</p>
+            <p>Communities is structurally and technically designed to resist
+                <a href="">enshittification</a>. It will be structured as a
+                multi-stakeholder cooperative, governed collaboratively by
+                you -- its users -- and us -- its workers.  The bylaws will be written so that
+                any major changes to them have to be ratified by a
+                super-majority of the workers and a majority of the active
+                users, ensuring that the structure can resist billionaire and
+                corporate takeover.</p>
+            <p>You will legally own the data you put on the platform and only
+                grant Communities the minimal license necessary to funtion. It
+                will be open source and open API, with clear and documented
+                exportable data structures, meaning anyone can build on top of
+                it and you can always leave and take your data with you.</p>
+            <p>Because we govern it and control it collectively, we can ensure
+            it stays healthy, helpful, and ours -- forever.</p>
         </article>
     )
 
---
+++ web-application/client/pages/about/views/Roadmap.js
@@ -0,0 +1,52 @@
+import React, { useEffect } from 'react'
+
+import './Roadmap.css'
+
+const Roadmap = function({}) {
+
+    useEffect(function() {
+        if ( document.location.hash ) {
+            document.querySelector(document.location.hash).scrollIntoView({
+                block: 'center'
+            })
+        }
+    }, [])
+
+    return (
+        <article className="roadmap-view">
+            <p>The Roadmap is our current plan for the platform's development.
+            The features described here are the things we've either already
+        built or would like to build.  <strong>The roadmap is subject to
+        frequent change based on feedback and new information.</strong>  It's
+        not a promise or a commitment, it's a loose plan, subject to
+        change.</p>
+            <h2>Copmleted</h2>
+            <ul>
+                <li><strong>Posts</strong> -- The ability to share Posts of up to 10,000 characters with your friends with either a single attached link or a single attached image.</li>
+                <li><strong>Comments</strong> -- You can leave Comments of up to 5,000 characters on Posts.</li>
+                <li><strong>Consensus based Reactions</strong> -- You can React to a Post using "like", "dislike", or "demote". Borrowing from consensus decision making, "like" and "dislike" are both positive for the Activity feed, "demote" is negative.</li>
+                <li><strong>Groups</strong> -- You can create Groups to organize your communities.</li>
+                <li><strong>Public Posts</strong> -- You can make Posts public to make them visible to anyone on your profile page.</li>
+                <li><strong>Post Sharing</strong> -- You can share public Posts to your own feed.</li>
+            </ul>
+            <h2>Future</h2>
+            <ul>
+                <li><strong>@mentions for Posts and Comments</strong> -- @mentions to allow users to directly reference and notify each other.</li>
+                <li><strong>Multi-factor Authentication</strong> -- Multi-factor authentication to protect user's accounts.</li>
+                <li><strong>Site Moderation Controls</strong> -- Moderation controls to allow site administrators and moderators to moderate the site.</li>
+                <li><strong>Community Moderation Controls</strong> -- Community moderation controls allowing you to moderation tags to post.  Also introduces demote thresholds to remove demoted posts from public feeds.</li> 
+                <li><strong>Location Based Feeds</strong> -- You can add your location and have feeds of public posts generated for your neighborhood, city, county, state/region, and nation.</li>
+                <li><strong>Events</strong> -- You can create events to gather with your communities in the real world.</li>
+                <li><strong>Post Image Galleries</strong> -- You can share galleries of images on Posts.</li>
+                <li><strong>Video Uploads</strong> -- You can upload videos to your Posts.</li>
+                <li><strong>Post Tagging</strong> -- You can add up to 5 Tags to a post.  You can browse feeds of posts for each Tag.  Tagging a post places it in the appropriate feed.</li>
+                <li><strong>Friend Lists</strong> -- You can organize your friends into private lists.</li>
+                <li><strong>Custom Feeds</strong> -- You can create custom feeds by combining and filtering all of the above elements: Friend Lists, Groups, Events, Location, and Tags.</li>
+            </ul>
+        </article>
+    )
+
+
+}
+
+export default Roadmap
