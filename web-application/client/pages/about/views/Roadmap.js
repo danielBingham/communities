ef30b37d7b0e7ca2041bfb362e0eac61625de598
Daniel Bingham <dbingham@theroadgoeson.com>
2025-07-17 16:45:37 -0400

    docs: updating the Roadmap

--- web-application/client/pages/about/views/Roadmap.js
+++ web-application/client/pages/about/views/Roadmap.js
@@ -28,17 +28,18 @@ const Roadmap = function({}) {
                 <li><strong>Groups</strong> -- You can create Groups to organize your communities.</li>
                 <li><strong>Public Posts</strong> -- You can make Posts public to make them visible to anyone on your profile page.</li>
                 <li><strong>Post Sharing</strong> -- You can share public Posts to your own feed.</li>
+                <li><strong>@mentions for Posts and Comments</strong> -- @mentions to allow users to directly reference and notify each other.</li>
+                <li><strong>Site Moderation Controls</strong> -- Moderation controls to allow site administrators and moderators to moderate the site.</li>
+                <li><strong>Group Moderation Controls</strong> -- Improved moderation controls for group administrators and moderators.</li>
             </ul>
             <h2>Future</h2>
             <ul>
-                <li><strong>@mentions for Posts and Comments</strong> -- @mentions to allow users to directly reference and notify each other.</li>
-                <li><strong>Multi-factor Authentication</strong> -- Multi-factor authentication to protect user's accounts.</li>
-                <li><strong>Site Moderation Controls</strong> -- Moderation controls to allow site administrators and moderators to moderate the site.</li>
-                <li><strong>Community Moderation Controls</strong> -- Community moderation controls allowing users to add moderation tags to posts.  Also introduces demote thresholds to remove demoted posts from public feeds.</li> 
                 <li><strong>Location Based Feeds</strong> -- You can add your location and have feeds of public posts generated for your neighborhood, city, county, state/region, and nation.</li>
                 <li><strong>Events</strong> -- You can create events to gather with your communities in the real world.</li>
                 <li><strong>Post Image Galleries</strong> -- You can share galleries of images on Posts.</li>
                 <li><strong>Video Uploads</strong> -- You can upload videos to your Posts.</li>
+                <li><strong>Multi-factor Authentication</strong> -- Multi-factor authentication to protect user's accounts.</li>
+                <li><strong>Community Moderation Controls</strong> -- Community moderation controls allowing users to add moderation tags to posts.  Also introduces demote thresholds to remove demoted posts from public feeds.</li> 
                 <li><strong>Post Tagging</strong> -- You can add up to 5 Tags to a post.  You can browse feeds of posts for each Tag.  Tagging a post places it in the appropriate feed.</li>
                 <li><strong>Friend Lists</strong> -- You can organize your friends into private lists.</li>
                 <li><strong>Custom Feeds</strong> -- You can create custom feeds by combining and filtering all of the above elements: Friend Lists, Groups, Events, Location, and Tags.</li>
