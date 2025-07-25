63dffb58be1b934a0dfaed1fc9b006056ad78c3a
Daniel Bingham <dbingham@theroadgoeson.com>
2025-07-25 09:40:16 -0400

    feat: scroll restoration

--- web-application/client/layouts/HeaderlessLayout.js
+++ web-application/client/layouts/HeaderlessLayout.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
+import { useScrollRestoration } from '/lib/hooks/useScrollRestoration'
+
 import './HeaderlessLayout.css'
 
 const HeaderlessLayout = function() {
+
+    useScrollRestoration()
 
     return (
         <main className="headerless">
--- web-application/client/layouts/MainLayout.js
+++ web-application/client/layouts/MainLayout.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
+import { useScrollRestoration } from '/lib/hooks/useScrollRestoration'
+
 import Header from '/components/header/Header'
 import Footer from '/components/header/Footer'
 
 import './MainLayout.css'
 
 const MainLayout = function() {
+    
+    useScrollRestoration()
 
     return (
         <>
