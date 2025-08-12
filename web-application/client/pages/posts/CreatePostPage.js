8a1b9e6d1a27a488c9991c2ca9ec39c654d047f8
Daniel Bingham <dbingham@theroadgoeson.com>
2025-08-12 09:23:11 -0400

    fix: reset entities after post creation
    Refs: #138

--- web-application/client/pages/posts/CreatePostPage.js
+++ web-application/client/pages/posts/CreatePostPage.js
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import { useSearchParams } from 'react-router-dom'
+
+import { resetEntities } from '/state/lib'
 
 import { Page, PageRightGutter, PageLeftGutter, PageBody } from '/components/generic/Page'
 import PostForm from '/components/posts/PostForm'
@@ -7,6 +10,13 @@ import PostForm from '/components/posts/PostForm'
 const CreatePostPage = function() {
 
     const [ searchParams, setSearchParams ] = useSearchParams()
+
+    const dispatch = useDispatch()
+    useEffect(() => {
+        return () => {
+            dispatch(resetEntities())
+        }
+    }, [])
 
     const postId = searchParams.get('postId')
     const groupId = searchParams.get('groupId')
