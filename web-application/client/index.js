33eab8bb02a3bc96fca4edb04bc32c5fa2acf9fc
Daniel Bingham <dbingham@theroadgoeson.com>
2025-09-10 16:03:26 -0400

    fix: wait for CapacitorUpdater to register notification before loading on mobile
    Refs: #234

--- web-application/client/index.js
+++ web-application/client/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 
 import { Provider } from 'react-redux'
 
+import { Capacitor } from '@capacitor/core'
 import { CapacitorUpdater } from '@capgo/capacitor-updater'
 
 import * as Sentry from "@sentry/react";
@@ -10,28 +11,57 @@ import * as Sentry from "@sentry/react";
 import App from './App'
 import store from './state/store'
 
-CapacitorUpdater.notifyAppReady()
+// We're on mobile.  Capacitor is handling updates.
+if ( Capacitor.getPlatform() === 'ios' || Capacitor.getPlatform() === 'android' ) {
+  CapacitorUpdater.notifyAppReady().then(function() {
+    let host = document.querySelector('meta[name="communities-host"]').content
+    let api = document.querySelector('meta[name="communities-api"]').content
+    let environment = document.querySelector('meta[name="communities-environment"]').content
 
-let host = document.querySelector('meta[name="communities-host"]').content
-let api = document.querySelector('meta[name="communities-api"]').content
-let environment = document.querySelector('meta[name="communities-environment"]').content
+    console.log(`Communities startup with::\n 
+      environment: "${environment}"\n
+      host: "${host}"\n
+      API: "${api}"\n
+    `)
 
-console.log(`Communities startup with::\n 
-  environment: "${environment}"\n
-  host: "${host}"\n
-  API: "${api}"\n
-`)
+    if ( environment === 'production' ) {
+      Sentry.init({
+        dsn: "https://3738393d51a9e4de9b7fe4b2bbb0bf56@o4509038666055680.ingest.us.sentry.io/4509038670249984"
+      });
+    }
 
-if ( environment === 'production' ) {
-  Sentry.init({
-    dsn: "https://3738393d51a9e4de9b7fe4b2bbb0bf56@o4509038666055680.ingest.us.sentry.io/4509038670249984"
-  });
+    const container = document.getElementById('root')
+    const root = createRoot(container)
+    root.render(
+        <Provider store={store}>
+            <App />
+        </Provider>
+    )
+  })
+} 
+// We're on the web platform.  Capacitor isn't handling updates.
+else {
+  let host = document.querySelector('meta[name="communities-host"]').content
+  let api = document.querySelector('meta[name="communities-api"]').content
+  let environment = document.querySelector('meta[name="communities-environment"]').content
+
+  console.log(`Communities startup with::\n 
+    environment: "${environment}"\n
+    host: "${host}"\n
+    API: "${api}"\n
+  `)
+
+  if ( environment === 'production' ) {
+    Sentry.init({
+      dsn: "https://3738393d51a9e4de9b7fe4b2bbb0bf56@o4509038666055680.ingest.us.sentry.io/4509038670249984"
+    });
+  }
+
+  const container = document.getElementById('root')
+  const root = createRoot(container)
+  root.render(
+      <Provider store={store}>
+          <App />
+      </Provider>
+  )
 }
-
-const container = document.getElementById('root')
-const root = createRoot(container)
-root.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
-)
