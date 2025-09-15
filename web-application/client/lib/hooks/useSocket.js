2d9ef14e6417a2550f1ef1368e074a66f7da905b
Daniel Bingham <dbingham@theroadgoeson.com>
2025-09-15 13:00:44 -0400

    fix: disconnect socket when the app is backgrounded

--- web-application/client/lib/hooks/useSocket.js
+++ web-application/client/lib/hooks/useSocket.js
@@ -1,11 +1,19 @@
 import { useEffect, useState, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+
+import { Capacitor } from '@capacitor/core'
+import { App } from '@capacitor/app'
+
+
 import { connect, disconnect } from '/state/socket'
 
 export const useSocket = function() {
     const currentUser = useSelector((state) => state.authentication.currentUser) 
+
     const isConnected = useSelector((state) => state.socket.isConnected)
     const inProgress = useSelector((state) => state.socket.inProgress)
+
+    const [isActive, setIsActive] = useState(true)
 
     const retryTimeout = useRef(null)
 
@@ -13,7 +21,30 @@ export const useSocket = function() {
 
     const dispatch = useDispatch()
     useEffect(function() {
-        if ( currentUser && ! isConnected && ! inProgress && retryTimeout.current === null) {
+
+        if ( Capacitor.getPlatform() === 'ios' || Capacitor.getPlatform() === 'android' ) {
+            App.addListener('appStateChanged', function(event) {
+                setIsActive(event.isActive)
+            })
+        }
+
+        return () => {
+            if ( Capacitor.getPlatform() === 'ios' || Capacitor.getPlatform === 'android' ) {
+                App.removeAllListeners()
+            }
+        }
+    }, [])
+
+    useEffect(function() {
+        if ( ! isActive ) {
+            if ( isConnected || inProgress ) {
+                dispatch(disconnect())
+            }
+        }
+    }, [ isActive, isConnected, inProgress])
+
+    useEffect(function() {
+        if ( currentUser && ! isConnected && ! inProgress && isActive && retryTimeout.current === null) {
             retryTimeout.current = setTimeout(() => {
                 dispatch(connect())
                 retryTimeout.current = null
@@ -32,7 +63,7 @@ export const useSocket = function() {
             // Reset the delay once we've successfully connected.
             setDelay(125)
         }
-    }, [ currentUser, isConnected, inProgress ])
+    }, [ currentUser, isConnected, inProgress, isActive ])
 
     return isConnected
 }
