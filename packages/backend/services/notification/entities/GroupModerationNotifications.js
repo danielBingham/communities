9eb339929a051ec0e5ab0888a02e51f63dd9aa89
Daniel Bingham <dbingham@theroadgoeson.com>
2025-11-17 16:02:20 -0500

    feat: Notify Group Moderators of Flagged Content
    Notify Group moderators when posts and comments in their groups are
    flagged.
    
    Refs: #316

--- packages/backend/services/notification/entities/GroupModerationNotifications.js
+++ packages/backend/services/notification/entities/GroupModerationNotifications.js
@@ -33,6 +33,8 @@ const PermissionService = require('../../PermissionService')
 module.exports = class GroupModerationNotifications {
     
     static notifications = [
+        'GroupModeration:create:post:moderator',
+        'GroupModeration:create:comment:moderator',
         'GroupModeration:update:post:status:rejected:author',
         'GroupModeration:update:comment:status:rejected:author'
     ]
@@ -57,57 +59,73 @@ module.exports = class GroupModerationNotifications {
         }
 
         context.post = await this.postDAO.getPostById(context.moderation.postId)
-        context.postIntro = context.post.content.substring(0,20)
 
         context.postAuthor = await this.userDAO.getUserById(context.post.userId, ['status']) 
 
-        context.group = null 
-        if ( context.post.groupId ) {
-            context.group = await this.groupDAO.getGroupById(context.post.groupId)
-            context.path = `/group/${context.group.slug}/${context.post.id}`
-            context.link = new URL(context.path, this.core.config.host).href
-        } else {
-            context.path = `/${context.postAuthor.username}/${context.post.id}`
-            context.link = new URL(context.path, this.core.config.host).href
-        }
+        context.group = await this.groupDAO.getGroupById(context.moderation.groupId)
+        context.path = `/group/${context.group.slug}/${context.post.id}`
+        context.link = new URL(context.path, this.core.config.host).href
 
-        if ( type === 'GroupModeration:update:comment:status:rejected:author' ) {
-            if ( ! ('postCommentId' in context.moderation) 
-                || context.moderation.postCommentId === undefined 
-                || context.moderation.postCommentId === null 
-            ) {
-                throw new ServiceError('missing-context',
-                    `Moderation notification missing postCommentId.`)
-            }
-
+        // We're moderating a comment.
+        if ( context.moderation.postCommentId !== undefined && context.moderation.postCommentId !== null ) {
             context.comment = await this.postCommentDAO.getPostCommentById(context.moderation.postCommentId)
-            context.commentIntro = context.comment.content.substring(0,20)
-
             context.commentAuthor = await this.userDAO.getUserById(context.comment.userId, ['status']) 
+        }
+    }
+
+    async create(currentUser, type, context, options) {
+        await this.ensureContext(currentUser, type, context)
+
+        const moderatorResults = await this.core.database.query(`
+            SELECT user_id FROM group_members WHERE group_members.group_id = $1 AND ( group_members.role = 'moderator' OR group_members.role = 'admin')
+        `, [ context.moderation.groupId ])
+
+        const moderatorIds = moderatorResults.rows.map((row) => row.user_id)
+
+        // A post is being flagged.
+        if (context.moderation.postId !== null && context.moderation.postCommentId === null ) {
+            for(const userId of moderatorIds) {
+                await this.notificationWorker.createNotification(userId, 'GroupModeration:create:post:moderator', context, options) 
+            }
+        } 
+        // A comment is being flagged.
+        else if ( context.moderation.postId !== null && context.moderation.postCommentId !== null ) {
+            for(const userId of moderatorIds) {
+                await this.notificationWorker.createNotification(userId, 'GroupModeration:create:comment:moderator', context, options) 
+            }
         }
     }
 
     async update(currentUser, type, context, options) {
         await this.ensureContext(currentUser, type, context)
 
-        if ( type === 'GroupModeration:update:post:status:rejected:author' ) {
-            // Don't send notifications to users who have lost the right to view
-            // their post.
-            const canViewPost = await this.permissionService.can(context.postAuthor, 'view', 'Post', { post: context.post, group: context.group })
-            if ( canViewPost !== true ) {
-                return
+        if ( context.moderation.status === 'rejected' ) {
+            // We're moderating a post.
+            if ( context.moderation.postId !== null && context.moderation.postCommentId === null ) {
+                // Don't send notifications to users who have lost the right to view
+                // their post.
+                const canViewPost = await this.permissionService.can(context.postAuthor, 'view', 'Post', { post: context.post, group: context.group })
+                if ( canViewPost !== true ) {
+                    return
+                }
+
+                await this.notificationWorker.createNotification(context.postAuthor.id, 
+                    'GroupModeration:update:post:status:rejected:author', context, options) 
             }
 
-            await this.notificationWorker.createNotification(context.postAuthor.id, 'GroupModeration:update:post:status:rejected:author', context, options) 
-        } else if ( type === 'GroupModeration:update:comment:status:rejected:author' ) {
-            // Don't send notifications to users who have lost the right to view
-            // their post.
-            const canViewPost = await this.permissionService.can(context.commentAuthor, 'view', 'Post', { post: context.post, group: context.group })
-            if ( canViewPost !== true ) {
-                return
-            }
+            // We're moderating a comment.
+            else if ( context.moderation.postId !== null && context.moderation.postCommentId !== null ) {
+                // Don't send notifications to users who have lost the right to view
+                // their post.
+                const canViewPost = await this.permissionService.can(context.commentAuthor, 'view', 'Post', { post: context.post, group: context.group })
+                if ( canViewPost !== true ) {
+                    return
+                }
 
-            await this.notificationWorker.createNotification(context.comment.userId, 'GroupModeration:update:comment:status:rejected:author', context, options) 
+                await this.notificationWorker.createNotification(context.comment.userId, 
+                    'GroupModeration:update:comment:status:rejected:author', context, options) 
+
+            }
         }
     }
 }
