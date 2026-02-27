94cbf2caa5d4d78b659258f9b29c0e159aa89765
Daniel Bingham <dbingham@theroadgoeson.com>
2026-02-27 11:01:56 -0500

    wip(feat): Dark Mode
    Combining AuthenticationNavigation and UserMenu.
    
    feat: #278

--- web-application/client/components/header/navigation/AuthenticationNavigation.js
+++ web-application/client/components/header/navigation/AuthenticationNavigation.js
@@ -1,13 +1,46 @@
-import React, { useState, useEffect, useRef } from 'react'
+/******************************************************************************
+ *
+ *  Communities -- Non-profit, cooperative social media 
+ *  Copyright (C) 2022 - 2024 Daniel Bingham 
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
 import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
-import { ChevronUpIcon, ChevronDownIcon } from '@heroicons/react/24/solid'
+import { 
+    UserCircleIcon,
+    EnvelopeIcon,
+    LockClosedIcon,
+    CreditCardIcon,
+    BellIcon,
+    ExclamationTriangleIcon,
+    PencilIcon,
+    ArrowRightOnRectangleIcon,
+    AdjustmentsHorizontalIcon,
+    MegaphoneIcon
+} from '@heroicons/react/24/outline'
 
-import Button from '/components/generic/button/Button'
+import { deleteAuthentication } from '/state/authentication'
+
+import { useRequest } from '/lib/hooks/useRequest'
+
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuBody, DropdownMenuSection, DropdownMenuItem } from '/components/ui/DropdownMenu'
+import Button from '/components/ui/Button'
 
 import UserProfileImage from '/components/users/UserProfileImage'
-import UserMenu from './UserMenu'
 
 import './AuthenticationNavigation.css'
 
@@ -17,68 +50,75 @@ import './AuthenticationNavigation.css'
  * @param {object} props    Standard React props object - empty.
  */
 const AuthenticationNavigation = function(props) {
-    
-    const [ menuVisible, setMenuVisible ] = useState(false)
-
-    const menuRef = useRef(null)
-
-    // ======= Request Tracking =====================================
-
-    // ======= Redux State ==========================================
 
     const currentUser = useSelector((state) => state.authentication.currentUser)
 
-    // ======= Actions and Event Handling ===========================
+    const [request, makeRequest] = useRequest()
 
     const navigate = useNavigate()
 
-    const toggleMenu = function(event) {
+    /**
+     * Handle a Logout request by dispatching the appropriate action.
+     *
+     * TODO Track this request and show an error if the attempt to deleteAuthentication
+     * fails.  Cleanup the request when we're done.
+     *
+     * @param {object} event - Standard event object.
+     */
+    const handleLogout = function(event) {
         event.preventDefault()
 
-        if ( props.setShowMobileMenu && menuVisible) {
-            props.setShowMobileMenu(false)
-        }
+        // Clear local storage so their drafts don't carry over to another
+        // login session.
+        localStorage.clear()
 
-        setMenuVisible( ! menuVisible )
+        makeRequest(deleteAuthentication())
     }
 
     const clickLogin = function(event) {
-        if ( props.setShowMobileMenu ) {
-            props.setShowMobileMenu(false)
-        }
         navigate('login')
     }
-
-    // ======= Effect Handling ======================================
-
-    useEffect(function() {
-        const onBodyClick = function(event) {
-            if ( menuRef.current && ! menuRef.current.contains(event.target) ) {
-                setMenuVisible(false)
-            } 
-        }
-        document.body.addEventListener('mousedown', onBodyClick)
-
-        return function cleanup() {
-            document.body.removeEventListener('mousedown', onBodyClick)
-        }
-    }, [ menuVisible, menuRef ])
 
     // ============= Render =======================
     
     if ( currentUser ) {
+        const isAdmin = currentUser.siteRole == 'admin' || currentUser.siteRole == 'superadmin'
         return (
-            <div ref={menuRef} id="authentication-navigation" className="authenticated">
-                <span className="logged-in-user">
-                    <a href="" className="no-close" onClick={toggleMenu}>
-                        <UserProfileImage userId={currentUser.id} noLink={true} />
-                        <span className="navigation-text">{ currentUser.name }</span></a></span>
-                <UserMenu visible={menuVisible} toggleMenu={toggleMenu} />
-            </div>
+            <DropdownMenu className="user-menu user-menu__authenticated" autoClose={true} >
+                <DropdownMenuTrigger className="user-menu__trigger logged-in-user">
+                    <UserProfileImage userId={currentUser.id} noLink={true} />
+                    <span className="navigation-text">{ currentUser.name }</span>
+                </DropdownMenuTrigger>
+                <DropdownMenuBody className="user-menu__body">
+                    <DropdownMenuSection>
+                        <DropdownMenuItem href={`/${currentUser.username}`}><UserCircleIcon />My Profile</DropdownMenuItem>
+                    </DropdownMenuSection>
+                    <DropdownMenuSection>
+                        <DropdownMenuItem href="/account/profile"><PencilIcon/>Edit Profile</DropdownMenuItem>
+                        <DropdownMenuItem href="/account/change-email"><EnvelopeIcon />Change Email</DropdownMenuItem>
+                        <DropdownMenuItem href="/account/change-password"><LockClosedIcon />Change Password</DropdownMenuItem>
+                    </DropdownMenuSection>
+                    <DropdownMenuSection>
+                        <DropdownMenuItem href="/account/preferences"><AdjustmentsHorizontalIcon /> Preferences</DropdownMenuItem>
+                        <DropdownMenuItem href="/account/notifications"><BellIcon />Notifications</DropdownMenuItem>
+                        <DropdownMenuItem href="/account/danger-zone"><ExclamationTriangleIcon /> Danger Zone</DropdownMenuItem>
+                    </DropdownMenuSection>
+                    { isAdmin && <DropdownMenuSection className="admin">
+                        <DropdownMenuItem href="/admin"><AdjustmentsHorizontalIcon/>Admin</DropdownMenuItem>
+                    </DropdownMenuSection> }
+                    <DropdownMenuSection>
+                        <DropdownMenuItem href="/account/contribute"><CreditCardIcon /> Contribute</DropdownMenuItem>
+                        <DropdownMenuItem href="/group/communities-feedback-and-discussion"><MegaphoneIcon /> Give Feedback</DropdownMenuItem>
+                    </DropdownMenuSection>
+                    <DropdownMenuSection last={true}> 
+                        <DropdownMenuItem className="logout" onClick={handleLogout}><ArrowRightOnRectangleIcon/>Log Out</DropdownMenuItem>
+                    </DropdownMenuSection>
+                </DropdownMenuBody>
+            </DropdownMenu>
         )
     } else {
         return (
-            <div id="authentication-navigation" className="not-authenticated">
+            <div className="user-menu user-menu__not-authenticated">
                 <Button type="primary" onClick={clickLogin}>Log In</Button>
                 <Button type="success" onClick={(e) => navigate('register')}>Sign Up</Button>
             </div>
--- web-application/client/components/header/navigation/UserMenu.js
+++ web-application/client/components/header/navigation/UserMenu.js
@@ -1,5 +1,23 @@
-import React, { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+/******************************************************************************
+ *
+ *  Communities -- Non-profit, cooperative social media 
+ *  Copyright (C) 2022 - 2024 Daniel Bingham 
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 import { useRequest } from '/lib/hooks/useRequest'
@@ -9,18 +27,17 @@ import {
     EnvelopeIcon,
     LockClosedIcon,
     CreditCardIcon,
-    Cog8ToothIcon,
     BellIcon,
     ExclamationTriangleIcon,
-    DocumentIcon,
     PencilIcon,
     ArrowRightOnRectangleIcon,
     AdjustmentsHorizontalIcon,
     MegaphoneIcon
 } from '@heroicons/react/24/outline'
 
-
 import { deleteAuthentication } from '/state/authentication'
+
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuSection, DropdownMenuItem } from '/components/ui/DropdownMenu'
 
 import './UserMenu.css'
 
