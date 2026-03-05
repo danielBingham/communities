8c87ca17c8646d61886f40ee49b826fca911c36b
Daniel Bingham <dbingham@theroadgoeson.com>
2026-03-05 13:25:44 -0500

    fix: spacing in FriendList

--- web-application/client/components/friends/list/FriendList.js
+++ web-application/client/components/friends/list/FriendList.js
@@ -103,7 +103,12 @@ const FriendList = function({ userId, params, noSearch, descriptor }) {
         const pageStart = ( query.meta.page-1) * query.meta.pageSize + 1
         const pageEnd = query.meta.count - (query.meta.page-1) * query.meta.pageSize > query.meta.pageSize ? ( query.meta.page * query.meta.pageSize ) : query.meta.count 
 
-        explanation = `${pageStart} to ${pageEnd} of ${query.meta.count} ${descriptor}`
+        explanation = (
+            <span>
+                <span className="friend-list__page">{pageStart} to {pageEnd}</span>
+                <span className="friend-list__total">of {query.meta.count} {descriptor}</span>
+            </span>
+        )
     }
 
     return (
