c2488d94848cb7d2bb6fa050396dfe567e8f0ef7
Daniel Bingham <dbingham@theroadgoeson.com>
2026-04-28 10:24:56 -0400

    wip(fix): add unique constraint to users.email and users.username
    Un-handled path needs a catch for the constraint error and we need a
    fall through throw.
    
    Refs: #486

--- packages/backend/services/UserService.js
+++ packages/backend/services/UserService.js
@@ -215,7 +215,37 @@ module.exports = class UserService {
             // Set their status here now that we've passed validation.
             user.status = 'invited'
 
-            await this.userDAO.insertUsers(user)
+            try { 
+                await this.userDAO.insertUsers(user)
+            } catch (error ) {
+                if ( 'constraint' in error ) {
+                    if ( error.constraint === 'users_email_unique' ) {
+                        errors.push({
+                            type: 'conflict',
+                            log: `User is already registered with that email.`,
+                            message: `A User is already registered with that email.  Please log in.`,
+                            context: {
+                                email: user.email
+                            }
+                        })
+                        return [ null, errors ]
+                    } else if ( error.constraint === 'users_username_unique' ) {
+                        errors.push({
+                            type: 'conflict',
+                            log: `User is already registered with that username.`,
+                            message: `A User is already registered with that username.`,
+                            context: {
+                                username: user.username
+                            }
+                        })
+                        return [ null, errors ]
+                    } else {
+                        throw error
+                    }
+                } else {
+                    throw error
+                }
+            }
 
             const createdUser = await this.userDAO.getUserByEmail(user.email, [ 'email', 'status' ])
             if ( createdUser === null ) {
@@ -404,6 +434,8 @@ module.exports = class UserService {
                             }
                         })
                         return [ null, errors ]
+                    } else {
+                        throw error
                     }
                 } else {
                     throw error
