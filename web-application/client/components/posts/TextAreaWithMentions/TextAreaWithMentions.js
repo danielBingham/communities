7a639451c851f6eebec8cba66ce8ef8bc6b5371f
Daniel Bingham <dbingham@theroadgoeson.com>
2026-06-10 15:01:24 -0400

    fix: minor performance optimization to the mention autocomplete
    Also adding explanatory comments for choices we're making.

--- web-application/client/components/posts/TextAreaWithMentions/TextAreaWithMentions.js
+++ web-application/client/components/posts/TextAreaWithMentions/TextAreaWithMentions.js
@@ -160,6 +160,9 @@ const TextAreaWithMentions = function({ value, setValue, postId, groupId, placeh
 
             // If we are mentioning and we just deleted the '@' character, then unset mentioning.
             if ( areMentioning ) {
+                // We're intentionally using `cursorIndex` here insead of
+                // `cursorIndex-1`, because that will be the index of the
+                // character we just deleted.
                 if ( text.length < value.length && value.at(cursorIndex) === '@' ) {
                     setAreMentioning(false)
                 }
@@ -205,6 +208,19 @@ const TextAreaWithMentions = function({ value, setValue, postId, groupId, placeh
     // Check the suggestions to see if there's an exact match (by name or
     // username) for the current mention.
     useEffect(() => {
+        // Intentionally not including `query` or `userDictionary` in the
+        // dependency array here.  We only want this to run when the mention
+        // changes (as they type) because we don't want to blow away anything
+        // they've typed later or screw with their cursor. It's okay if query
+        // or userDictionary are a bit stale, since they'll probably get
+        // updated on the next run.  By the time they've typed a complete
+        // mention, it's highly likely we'll have the user in question in the
+        // query or the userDictionary (unless they are on a really slow
+        // connection).  In the case that they don't, it's better that the
+        // autocomplete silently fail than it fire after they've moved on.
+        //
+        // They can always back space and correct the mention later.
+
         // Slice off the '@' character, so we can make an exact comparison.
         const mentionText = currentMention.substring(1)
 
@@ -214,6 +230,17 @@ const TextAreaWithMentions = function({ value, setValue, postId, groupId, placeh
                 const user = userDictionary[id]
                 if ( mentionText.toLowerCase() === user.name.toLowerCase() || mentionText.toLowerCase() === user.username.toLowerCase() ) {
                     selectSuggestion(index)
+                    break
+                }  
+
+                // If the mentionText is not an exact match for the substring
+                // of its length, then we're in to the fuzzy matches and there
+                // shouldn't be any exact matches further down the list.  We
+                // can break the loop.
+                else if ( mentionText.toLowerCase() !== user.name.toLowerCase().substring(0, mentionText.length)
+                    && mentionText.toLowerCase() !== user.username.toLowerCase().substring(0, mentionText.length)
+                ) {
+                    break
                 }
             }
         }
