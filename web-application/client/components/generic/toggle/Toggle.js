4a58efd384194556a524485c95a03bbdd9f16a8e
Daniel Bingham <dbingham@theroadgoeson.com>
2026-06-15 14:30:20 -0400

    wip(feat): accessibility
    Refs: #431

--- web-application/client/components/generic/toggle/Toggle.js
+++ web-application/client/components/generic/toggle/Toggle.js
@@ -1,4 +1,23 @@
-import React, { useState, useEffect, useId } from 'react'
+/******************************************************************************
+ *
+ *  Communities -- Non-profit, cooperative social media 
+ *  Copyright (C) 2022 - 2024 Daniel Bingham 
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+import { useId } from 'react'
 
 import './Toggle.css'
 
