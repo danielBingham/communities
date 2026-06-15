80de694ef598a8541425a46f4f8e4660cb194d3c
Daniel Bingham <dbingham@theroadgoeson.com>
2026-06-15 13:29:18 -0400

    wip(feat): accessibility
    Use a generate id in TextBox to match the label.
    
    Refs: #431

--- web-application/client/components/generic/text-box/TextBox.js
+++ web-application/client/components/generic/text-box/TextBox.js
@@ -1,9 +1,29 @@
-import React, { useId } from 'react'
+/******************************************************************************
+ *
+ *  Communities -- Non-profit, cooperative social media 
+ *  Copyright (C) 2022 - 2024 Daniel Bingham 
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+import { useId } from 'react'
 
 import './TextBox.css'
 
 const TextBox = function({ label, explanation, name, className, value, onChange, onBlur, onFocus, error }) {
 
+    const textareaId = useId()
     const explanationId = useId()
     const errorId = useId()
 
@@ -27,10 +47,10 @@ const TextBox = function({ label, explanation, name, className, value, onChange,
 
     return (
         <div className={`text-box ${className ? className : ''}`}>
-            { label && <label htmlFor={name}>{ label }</label> }
+            { label && <label htmlFor={`${name}-${textareaId}`}>{ label }</label> }
             { explanation && <p id={explanationId} className="text-box_explanation">{explanation}</p> }
             <textarea name={name} 
-                id={name}
+                id={`${name}-${textareaId}`}
                 value={value} 
                 aria-describedby={`${ explanation ? explanationId : '' } ${ error ? errorId : '' }`.trim() || undefined}
                 aria-invalid={ error ? true : undefined }
