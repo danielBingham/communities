c8d7f9e139150246194270f56be66853921fc503
Daniel Bingham <dbingham@theroadgoeson.com>
2026-06-17 14:11:26 -0400

    wip(feat): accessibility
    Fixing a bug where the search control would be unfocused after typing.
    
    Refs: #431

--- web-application/client/state/UserRelationship/thunks.js
+++ web-application/client/state/UserRelationship/thunks.js
@@ -1,7 +1,27 @@
+/******************************************************************************
+ *
+ *  Communities -- Non-profit, cooperative social media 
+ *  Copyright (C) 2022 - 2024 Daniel Bingham 
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
 import * as qs from 'qs'
 
 import { makeRequest } from '/state/lib/makeRequest'
 import { setRelationsInState } from '/state/lib/relations'
+import { resetEntities } from '/state/lib'
 
 import {
     setUserRelationshipsInDictionary, setUserRelationshipNull, removeUserRelationship, 
@@ -50,9 +70,9 @@ export const postUserRelationships = function(relationship) {
     return function(dispatch, getState) {
         return dispatch(makeRequest('POST', `/user/${encodeURIComponent(relationship.userId)}/relationships`, relationship,
             function(response) {
-                dispatch(setUserRelationshipsInDictionary({ entity: response.entity }))
-
-                dispatch(setRelationsInState(response.relations))
+                // When a relationship is created we need to reset and requery
+                // entities.
+                dispatch(resetEntities())
             }
         ))
     }
@@ -104,9 +124,9 @@ export const patchUserRelationship = function(relationship) {
     return function(dispatch, getState) {
         return dispatch(makeRequest('PATCH', `/user/${encodeURIComponent(relationship.userId)}/relationship/${encodeURIComponent(relationship.relationId)}`, relationship,
             function(response) {
-                dispatch(setUserRelationshipsInDictionary({ entity: response.entity }))
-
-                dispatch(setRelationsInState(response.relations))
+                // When a relationship is patched we need to reset and requery
+                // entities.
+                dispatch(resetEntities())
 
             }
         ))
@@ -129,9 +149,9 @@ export const deleteUserRelationship = function(relationship) {
     return function(dispatch, getState) {
         return dispatch(makeRequest('DELETE', `/user/${encodeURIComponent(relationship.userId)}/relationship/${encodeURIComponent(relationship.relationId)}`, null,
             function(response) {
-                dispatch(removeUserRelationship({ entity: response.entity }))
-
-                dispatch(setRelationsInState(response.relations))
+                // When a relationship is deleted we need to reset and requery
+                // entities.
+                dispatch(resetEntities())
             } 
         ))
     }
